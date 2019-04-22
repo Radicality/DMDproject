4291a63d5508cc15a55bde26187ea0b0cc33ef42
DeadmanIQ445 <35527371+DeadmanIQ445@users.noreply.github.com>
2019-04-22 16:36:15 +0300

    Added traverse

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 from arango import ArangoClient
 
 client = ArangoClient()
@@ -19,40 +21,83 @@ def init():
     if not db.has_collection('Projects'):
         db.create_collection(name='Projects')
     global projects
-    projects = db.collection('Project')
+    projects = db.collection('Projects')
 
     if not db.has_collection('Tasks'):
         db.create_collection(name='Tasks')
     global tasks
     tasks = db.collection('Tasks')
 
-    global project_graph
-    if db.has_graph('Project'):
-        project_graph = db.graph('Project')
+    global project_user
+    if db.has_graph('Project-User'):
+        project_user = db.graph('Project-User')
     else:
-        project_graph = db.create_graph('Project')
+        project_user = db.create_graph('Project-User')
+
+    global task_user
+    if db.has_graph('Task-User'):
+        task_user = db.graph('Task-User')
+    else:
+        task_user = db.create_graph('Task-User')
+
+    global task_project
+    if db.has_graph('Task-Project'):
+        task_project = db.graph('Task-Project')
+    else:
+        task_project = db.create_graph('Task-Project')
 
     global edge_w_o_p
-    if project_graph.has_edge_definition('Project'):
-        edge_w_o_p = project_graph.edge_collection('Project')
+    if project_user.has_edge_definition('Project-User'):
+        edge_w_o_p = project_user.edge_collection('Project-User')
 
     else:
-        edge_w_o_p = project_graph.create_edge_definition(
-            edge_collection='Project',
+        edge_w_o_p = project_user.create_edge_definition(
+            edge_collection='Project-User',
             from_vertex_collections=['User'],
             to_vertex_collections=['Projects']
         )
 
     global edge_w_o_t
-    if project_graph.has_edge_definition('Task'):
-        edge_w_o_t = project_graph.edge_collection('Task')
+    if task_user.has_edge_definition('Task-User'):
+        edge_w_o_t = task_user.edge_collection('Task-User')
     else:
-        edge_w_o_t = project_graph.create_edge_definition(
-            edge_collection='Task',
+        edge_w_o_t = task_user.create_edge_definition(
+            edge_collection='Task-User',
             from_vertex_collections=['User'],
             to_vertex_collections=['Tasks']
         )
 
+    global edge_t_w_p
+    if task_project.has_edge_definition('Task-Project'):
+        edge_t_w_p = task_project.edge_collection('Task-Project')
+    else:
+        edge_t_w_p = task_project.create_edge_definition(
+            edge_collection='Task-Project',
+            from_vertex_collections=['User'],
+            to_vertex_collections=['Tasks']
+        )
+
+    generate()
+
+def generate():
+    for i in range(100):
+        add_user('name'+str(i), 'mail'+str(i)+'@mail.ma')
+    for i in range(100):
+        add_project('nameOfProject'+str(i), 'project_key'+str(i))
+    for i in range(100):
+        add_task('nameOfTask'+str(i), str(i//24)+'-00', str((i+1)//24)+'-00', 'in process')
+    for i in range(1, 99):
+        user = users.get({'_key': 'mail'+str(i)+'@mail.ma'})
+        for x in range(random.randint(2, 4)):
+            j = random.randint(1, 99)
+            project = projects.get({'_key': 'project_key'+str(j)})
+            insert_works_on_project(user['_id'], project['_id'])
+    for i in range(1, 99):
+        user = users.get({'_key': 'mail'+str(i)+'@mail.ma'})
+        for x in range(random.randint(2, 4)):
+            j = random.randint(1, 99)
+            project = tasks.get({'_key': 'nameOfTask'+str(j)})
+            insert_works_on_task(user['_id'], project['_id'])
 
 def add_user(name, email):
     user = db.insert_document('User', {'_key': email, 'name': name, 'email': email})
@@ -65,7 +110,7 @@ def add_project(name, project_key):
 
 
 def add_task(name, start, end, status):
-    task = db.insert_document('Tasks', {'name': name, 'start': start, 'end': end, 'status': status})
+    task = db.insert_document('Tasks', {'_key': name.strip(' '), 'name': name, 'start': start, 'end': end, 'status': status})
     return task
 
 
@@ -82,7 +127,27 @@ def insert_works_on_task(user, task):
         '_to': task
     })
 
+
+def traverse_works_on_tasks():
+    trav = task_user.traverse(
+        start_vertex='User/mail6@mail.ma',
+        direction='outbound',
+        strategy='bfs',
+        edge_uniqueness='global',
+        vertex_uniqueness='global',
+    )
+    return trav['vertices'][1:]
+
+
+def traverse_works_on_project():
+    trav = project_user.traverse(
+        start_vertex='User/mail6@mail.ma',
+        direction='outbound',
+        strategy='bfs',
+        edge_uniqueness='global',
+        vertex_uniqueness='global',
+    )
+    return trav['vertices'][1:]
+
 init()
-us = add_user('name', 's10@r.v')
-task = add_task('do', '23-40', '23-45', 'in process')
-insert_works_on_task(us['_id'], task['_id'])
+print(traverse_works_on_tasks())
